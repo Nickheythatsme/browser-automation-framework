0e87c457a1e9ff668b094e0b116f5db927211e8f
Nicholas Grout <njgrout@gmail.com>
2020-02-08 11:52:43 -0800

    modified:   package.json 	modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ Driver.initialize({ headless: true }).then(async driver => {
         .screenshot()
         .click('a[href="https://stackexchange.com/questions?tab=hot"]', { timeout: 3000 })
         .click('f')
+        .click('a')
         .close()
         .perform()
 });
